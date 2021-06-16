ea76d4f6a55206db96f789b38d2b1ec8cc50a64c
Tanay Dilip Kewalramani <tanayke@cybage.com>
2021-06-16 11:28:48 +0530

    some user and location apis demo

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const { User, Location } = require('../../models');
+
 const router = express.Router();
 
 // @route GET api/user
-// @desc  test route
+// @desc  get all users
 // @access Public
-router.get('/', (req, res) => res.send('User Router'));
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll({ include: 'location' });
+    return res.json(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
+});
+
+// @route POST api/user
+// @desc  create a user
+// @access Public
+router.post('/', async (req, res) => {
+  const { role, name, email, phone, password, isApproved, location_id } =
+    req.body;
+
+  try {
+    const user = await User.create({
+      role,
+      name,
+      email,
+      phone,
+      password,
+      isApproved,
+      location_id,
+    });
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
