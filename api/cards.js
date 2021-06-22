11175d3b65976a4d4a65875e7fa48bbfc440442d
yash <74835997+yash123-code@users.noreply.github.com>
2021-06-22 18:46:59 +0530

    card.js modified

--- api/cards.js
+++ api/cards.js
@@ -1,10 +1,31 @@
 const express = require('express');
 
 const router = express.Router();
+const { Card } = require('../models');
+
+router.post('/', async (req, res) => {
+  const { type, content, articleId } = req.body;
+  card = new Card({
+    type,
+    content,
+    cardsOrder,
+    article_id: articleId,
+  });
+  await card.save();
+  return res.json({ message: 'card added succesfully' });
+});
 
 // @route GET api/cards
 // @desc  test route
 // @access Public
-router.get('/', (req, res) => res.send('cards Router'));
+router.get('/:id', async (req, res) => {
+  try {
+    const cards = await Card.findAll({ where: { article_id: req.params.id } });
+    return res.json(cards);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
+});
 
 module.exports = router;
