74709d3df4356e4e8cde21ebcdfebf4d82866b2d
Triveni Pratik Patel (Chaudhari) <trivenic@cybage.com>
2021-06-24 16:14:19 +0530

    article catch block

--- api/article.js
+++ api/article.js
@@ -31,20 +31,24 @@ router.get('/', (req, res) => res.send('arcticle Router'));
 
 router.post('/', upload, async (req, res) => {
   const { title, description, categoryId, reporterId, locationId } = req.body;
-  const article = new Article({
-    title,
-    description,
-    thumbnailImage: '/thumbnail/' + req.file.filename,
-    viewCount: 0,
-    uploadDateTime: new Date(),
-    isActive: 0,
-    category_id: categoryId,
-    reporter_id: reporterId,
-    location_id: locationId,
-  });
-  await article.save();
-  return res.status(201).json(article);
-  // return res.json({ message: 'article added successfully' });
+  console.log(locationId);
+  try {
+    const article = await Article.create({
+      title,
+      description,
+      thumbnailImage: '/thumbnail/' + req.file.filename,
+      viewCount: 0,
+      uploadDateTime: new Date(),
+      isActive: 0,
+      category_id: categoryId,
+      reporter_id: reporterId,
+      location_id: locationId,
+    });
+    return res.status(201).json(article);
+  } catch (err) {
+    // console.log(err);
+    return res.status(400).json(err.message);
+  }
 });
 
 module.exports = router;
