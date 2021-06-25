450c99207b5d5d6096dafc3b4d610ee9062453bf
Triveni Pratik Patel (Chaudhari) <trivenic@cybage.com>
2021-06-25 16:25:32 +0530

    location with ariticle added

--- api/article.js
+++ api/article.js
@@ -1,10 +1,14 @@
+/* eslint-disable use-isnan */
+/* eslint-disable no-const-assign */
 /* eslint-disable prefer-template */
 /* eslint-disable consistent-return */
 const express = require('express');
+const { createReadStream } = require('fs');
 
 const multer = require('multer');
 const path = require('path');
 const { Article } = require('../models');
+const { Location } = require('../models');
 
 const router = express.Router();
 // @route GET api/articles
@@ -12,9 +16,14 @@ const router = express.Router();
 // @access Public
 
 const Storage = multer.diskStorage({
-  destination: './public/thumbnail',
-
-  // By default, multer removes file extensions so let's add them back
+  destination: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname === 'thumbnailImage'
+        ? './public/thumbnail'
+        : './public/cards'
+    );
+  },
   filename: (req, file, cb) => {
     cb(
       null,
@@ -30,25 +39,52 @@ const upload = multer({
 router.get('/', (req, res) => res.send('arcticle Router'));
 
 router.post('/', upload, async (req, res) => {
-  // const { title, description, categoryId, reporterId, locationId ,cards } = req.body;
   console.log(req.body);
-  //   try {
-  //     const article = await Article.create({
-  //       title,
-  //       description,
-  //       thumbnailImage: '/thumbnail/' + req.file.filename,
-  //       viewCount: 0,
-  //       uploadDateTime: new Date(),
-  //       isActive: 0,
-  //       category_id: categoryId,
-  //       reporter_id: reporterId,
-  //       location_id: locationId,
-  //     });
-  //     return res.status(201).json(article);
-  //   } catch (err) {
-  //     // console.log(err);
-  //     return res.status(400).json(err.message);
-  //   }
+  console.log(req.files);
+  console.log(req.files.length);
+
+  req.files.forEach((file,i)=>{
+     console.log(req.body.card.+i+.type);
+     // console.log(req.body.card.+i+.content);
+  })
+
+  const {
+    title,
+    description,
+    state,
+    city,
+    locality,
+    categoryId,
+    reporterId,
+    isNewlocation,
+  } = req.body;
+
+  try {
+    const location =
+      isNewlocation === 'true'
+        ? await Location.create({
+            locality,
+            city,
+            state,
+          })
+        : undefined;
+
+    const article = await Article.create({
+      title,
+      description,
+      thumbnailImage: '/thumbnail/' + req.files[0].filename,
+      viewCount: 0,
+      uploadDateTime: new Date(),
+      isActive: 0,
+      category_id: categoryId,
+      reporter_id: reporterId,
+      location_id: isNewlocation === 'true' ? location.id : locality,
+    });
+    return res.status(201).json(article);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
 });
 
 module.exports = router;
